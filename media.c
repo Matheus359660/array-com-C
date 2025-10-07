de05ccd4f3c31fe68cfb6fbf75ed8e2c2f6ffc4b
Matheus359660 <goncalves.santos.matheus1601@escola.pr.gov.br>
2025-10-07 13:58:05 +0000

    em andamento

---
+++ media.c
@@ -0,0 +1,15 @@
+# include <stdio.h>
+
+int main(){
+    char nome[50];
+    float notal, nota2, nota3, media;
+    printf("%s", nome);
+
+    printf("Digte o nome do aluno: ");
+    scanf("%s", nome);
+
+    printf("Digite as 3 notas: ");
+    scanf("%f %f %f",&nota1, &nota2, &nota3);
+
+    media = (nota01 )
+}
